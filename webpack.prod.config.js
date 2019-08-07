ce61dd99f9dc6a6ff1c9b5c4f60bde37f5d3d201
anilseyrek@gmail.com <anilseyrek@gmail.com>
2019-08-07 06:42:44 +0300

    update webpack.config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -112,12 +112,12 @@ baseConfig[1].plugins = [
   }),
   new SpritesmithPlugin({
       src: {
-        cwd: path.resolve(__dirname, 'static/images/'),
+        cwd: path.resolve(__dirname, 'assets/images/'),
         glob: '*.png'
       },
       target: {
-        image: path.resolve(__dirname, 'static/images/spritesmith-generated/sprite.png'),
-        css: path.resolve(__dirname, 'static/sass/vendor/spritesmith.scss')
+        image: path.resolve(__dirname, 'assets/images/spritesmith-generated/sprite.png'),
+        css: path.resolve(__dirname, 'assets/sass/vendor/spritesmith.scss')
       },
       retina: '@2x'
   }),
