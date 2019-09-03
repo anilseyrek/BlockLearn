be9c622f6b44914eb21544c65f23b776f3080088
anilseyrek@gmail.com <anilseyrek@gmail.com>
2019-09-03 22:13:27 +0300

    make migrations again

--- users/models.py
+++ users/models.py
@@ -33,7 +33,6 @@ class User(AbstractBaseUser, PermissionsMixin, IndexedTimeStampedModel):
         return self.email
         '''
 
-
 class UserProfile(models.Model):
     """
     Class to define external properties of a user.
@@ -46,15 +45,15 @@ class UserProfile(models.Model):
         return self.user.__str__()
 
 
-# def create_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     """
     Signal listener for User model. Triggers after a user is created.
     """
-#    if created:
-#        UserProfile.objects.create(user=instance)
+    if created:
+        UserProfile.objects.create(user=instance)
 
 # Connects the signal listener to the user.
-# post_save.connect(create_user_profile, sender=User)
+post_save.connect(create_user_profile, sender=User)
 
 class Progress(models.Model):
     course_URL = models.CharField(max_length=255, default="/static/courses/blockchain.html")
