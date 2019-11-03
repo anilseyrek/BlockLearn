5af9f9dd426ffac077aca4d51828e4837bd27588
anilseyrek@gmail.com <anilseyrek@gmail.com>
2019-11-04 02:10:10 +0300

    correct errors in auth process of React app

--- assets/js/app/blocklearn/actions/auth.js
+++ assets/js/app/blocklearn/actions/auth.js
@@ -11,30 +11,33 @@ export const loadUser = () => {
 
         if (token) {
             headers["authorization"] = `Token ${token}`;
+            return fetch("/api/auth/user/", {headers, })
+                .then(res => {
+                    if (res.status < 500) {
+                        return res.json().then(data => {
+                            return {status: res.status, data};
+                        })
+                    } else {
+                        console.log("Server Error!");
+                        throw res;
+                    }
+                })
+                .then(res => {
+                    if (res.status === 200) {
+                        dispatch({type: 'USER_LOADED', user: res.data });
+                        return res.data;
+                    } else if (res.status >= 400 && res.status < 500) {
+                        dispatch({type: "AUTHENTICATION_ERROR", data: res.data});
+                        throw res; // FIXME not a good idea to throw an unknown data (Usually an error should be thrown)
+                    }
+                })
+                .catch(err => {
+                    dispatch({ type: "APPLICATION_ERROR", err });
+                });
         }
-        return fetch("/api/auth/user/", {headers, })
-            .then(res => {
-                if (res.status < 500) {
-                    return res.json().then(data => {
-                        return {status: res.status, data};
-                    })
-                } else {
-                    console.log("Server Error!");
-                    throw res;
-                }
-            })
-            .then(res => {
-                if (res.status === 200) {
-                    dispatch({type: 'USER_LOADED', user: res.data });
-                    return res.data;
-                } else if (res.status >= 400 && res.status < 500) {
-                    dispatch({type: "AUTHENTICATION_ERROR", data: res.data});
-                    throw res.data; // FIXME not a good idea to throw an unknown data (Usually an error should be thrown)
-                }
-            })
-            .catch(err => {
-                dispatch({ type: "APPLICATION_ERROR", err });
-            });
+        else {
+          return dispatch({type: "USER_NOT_LOADED"});;
+        }
     }
 }
 
